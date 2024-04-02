a30daadde5b60908d881d46fe0e0415cfe677309
bumasello <bumasello@gmail.com>
2024-04-01 23:41:17 -0300

    mais uma atualizacao

--- lua/bmasello/plugins/colorscheme.lua
+++ lua/bmasello/plugins/colorscheme.lua
@@ -7,6 +7,15 @@ return {
 	--		vim.cmd([[colorscheme moonfly]])
 	--	end,
 	--},
+	--{
+	--	"craftzdog/solarized-osaka.nvim",
+	--	lazy = false,
+	--	priority = 1000,
+	--	opts = {},
+	--	config = function()
+	--		vim.cmd([[colorscheme solarized-osaka]])
+	--	end,
+	--},
 	{
 		"svrana/neosolarized.nvim",
 		dependencies = { "tjdevries/colorbuddy.nvim" },
